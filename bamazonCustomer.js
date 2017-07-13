fd8c85f6280b191bd1ae76c668fd9201c604b6a8
Tyler Peterson <trpeterson42@gmail.com>
2017-07-13 10:08:54 -0700

    ...

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
+var mysql = require('mysql');
 var prompt = require('prompt');
 var inquirer = require('inquirer');
-var mysql = require('mysql');
 var Table = require('cli-table');
 var connection = mysql.createConnection({
 	host:'localhost',
@@ -44,25 +44,25 @@ var checkAndBuy2 = function() {
                     return false;
                 }
             }
-        }]).then(function(answer) {
-            var chosenId = answer.Item_id - 1
-            var chosenProduct = res[chosenId]
-            var chosenQuantity = answer.Quantity
-            if (chosenQuantity < res[chosenId].Stock_Quantity) {
-                console.log("Your total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].Product_Name + " is: " + res[chosenId].Price.toFixed(2) * chosenQuantity);
-                connection.query("UPDATE products SET ? WHERE ?", [{
-                    StockQuantity: res[chosenId].StockQuantity - chosenQuantity
-                }, {
-                    id: res[chosenId].id
-                }], function(err, res) {
-                    //console.log(err);
-                    checkAndBuy2();
-                });
+//         }]).then(function(answer) {
+//             var chosenId = answer.Item_id - 1
+//             var chosenProduct = res[chosenId]
+//             var chosenQuantity = answer.Quantity
+//             if (chosenQuantity < res[chosenId].Stock_Quantity) {
+//                 console.log("Your total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].Product_Name + " is: " + res[chosenId].Price.toFixed(2) * chosenQuantity);
+//                 connection.query("UPDATE products SET ? WHERE ?", [{
+//                     StockQuantity: res[chosenId].StockQuantity - chosenQuantity
+//                 }, {
+//                     id: res[chosenId].id
+//                 }], function(err, res) {
+//                     //console.log(err);
+//                     checkAndBuy2();
+//                 });
 
-            } else {
-                console.log("Sorry, insufficient Quanity at this time. All we have is " + res[chosenId].Stock_Quantity + " in our Inventory.");
-                checkAndBuy2();
-            }
+//             } else {
+//                 console.log("Sorry, insufficient Quanity at this time. All we have is " + res[chosenId].Stock_Quantity + " in our Inventory.");
+//                 checkAndBuy2();
+//             }
         })
     })
 }
